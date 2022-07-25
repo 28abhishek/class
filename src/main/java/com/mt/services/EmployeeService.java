71b9573feed1d65505359bd251781dc8e0dba5d3
abhishek <28.abhishekn@gmail.com>
2022-07-25 13:56:53 +0530

    first commit

---
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.mt.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeService {
+
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "youtude made easy to learn DevOps");
+		js.put("Calling Name", "abhishek");
+		js.put("DOB", "28-feb-1998");
+		js.put("Hobbies", "Writing Technical Blogs, Teaching, YouTube, aws");
+		return js.toString();
+}
+}
